348fcc45162d26311873bbe6cee5dc47157a998e
Guido Trucco <gftrucco@gmail.com>
2016-11-05 16:43:49 -0300

    Ataque y AlgoMon para empezar

---
+++ src/clases/AlgoMon.java
@@ -0,0 +1,41 @@
+package clases;
+
+import java.util.List;
+
+public abstract class AlgoMon {
+	private int vida;
+	private List<Ataque> ataques;
+	private Tipo tipo;
+	
+	public void atacar(AlgoMon otro, Ataque ataque){
+		
+	}
+	
+	public void disminuirVida(int puntos){
+		this.setVida(this.getVida() - puntos);
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public List<Ataque> getAtaques() {
+		return ataques;
+	}
+
+	public void setAtaques(List<Ataque> ataques) {
+		this.ataques = ataques;
+	}
+
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+}
---
+++ src/clases/Ataque.java
@@ -0,0 +1,47 @@
+package clases;
+
+import excepciones.AtaquesAgotadosException;
+
+public abstract class Ataque {
+	
+	private int potencia;
+	private int cantidad;
+	private Tipo tipo;
+		
+	private Ataque(int potencia, int cantidad, Tipo tipo){
+		this.setPotencia(potencia);
+		this.setCantidad(cantidad);
+		this.setTipo(tipo);
+	}
+	
+	public void atacar(AlgoMon atacante, AlgoMon atacado){
+		if(this.getCantidad() == 0)
+			throw new AtaquesAgotadosException();
+		double multiplicador = this.getTipo().obtenerMultiplicador(atacado.getTipo());
+		atacado.disminuirVida((int)(this.getPotencia()*multiplicador));
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+}
