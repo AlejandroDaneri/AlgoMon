2da8c1e6086af160ecf85553793f794f79719625
nicolaslongo <longo.gnr@hotmail.com>
2016-11-12 16:44:15 -0300

    ChanseyTest.java actualizado

--- test/tests/ChanseyTest.java
+++ test/tests/ChanseyTest.java
@@ -3,8 +3,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import clases.Bulbasaur;
 import clases.Chansey;
+import clases.Charmander;
+import clases.Jigglypuff;
 import clases.Tipo;
+import excepciones.AtaquesAgotadosException;
 
 
 public class ChanseyTest {
@@ -29,7 +33,6 @@ public class ChanseyTest {
 		String ataque3 = "LatigoCepa";
 		String ataque4 = "Chupavidas";
 
-		
 		assertEquals(chansey.getTipo(), Tipo.NORMAL);
 		assertEquals(chansey.getVida(), 130);
 		assertEquals(chansey.tieneAtaque(ataque1), true);
@@ -37,4 +40,47 @@ public class ChanseyTest {
 		assertEquals(chansey.tieneAtaque(ataque3), true);
 		assertEquals(chansey.tieneAtaque(ataque4), false);
 	}
+	
+	@Test
+	public void testAlgoMonDormido() {
+		
+		Chansey chansey = new Chansey();
+		Jigglypuff jigglypuff = new Jigglypuff();
+		try {
+			jigglypuff.atacar(chansey, "Canto");
+		} catch (AtaquesAgotadosException e) { }
+		assertEquals(false, chansey.puedeAtacar());
+	}
+	
+	@Test
+	public void testAlgoMonQuemado() {
+		
+		Chansey chansey = new Chansey();
+		Charmander charmander = new Charmander();
+		try {
+			charmander.atacar(chansey, "Fogonazo");
+			chansey.nuevoTurno();
+			chansey.atacar(charmander, "AtaqueRapido");
+		} catch (AtaquesAgotadosException e) { }
+		assertEquals(115, chansey.getVida());
+	}
+
+	@Test
+	public void testAlgoMonMuerto() {
+		Chansey chansey = new Chansey();
+		Charmander charmander = new Charmander();
+		try {
+			charmander.atacar(chansey, "Brasas");
+			charmander.atacar(chansey, "Brasas");
+		} catch (AtaquesAgotadosException e) { }
+		assertEquals(false, chansey.estaMuerto());
+
+		try {
+			for(int i=0; i<7;i++){
+				charmander.atacar(chansey, "Brasas");
+			}
+		} catch (AtaquesAgotadosException e) { }
+		assertEquals(true, chansey.estaMuerto());
+	}
+
 }
